81d276c0323aac4e96a44a4c735cfe33daddd8fb
Eric Heiden <eheiden@nvidia.com>
2025-06-27 17:44:17 +0200

    Fix thickness consideration in collision module
    Signed-off-by: Eric Heiden <eheiden@nvidia.com>

--- newton/solvers/featherstone/solver_featherstone.py
+++ newton/solvers/featherstone/solver_featherstone.py
@@ -20,6 +20,7 @@ from newton.sim import Contacts, Control, Model, State, eval_fk
 
 from ..euler.kernels import (
     eval_bending_forces,
+    eval_body_contact_forces,
     eval_muscle_forces,
     eval_particle_body_contact_forces,
     eval_rigid_contacts,
@@ -398,7 +399,6 @@ class FeatherstoneSolver(SolverBase):
                             state_aug.body_v_s,
                             model.body_com,
                             model.shape_materials,
-                            model.shape_geo,
                             model.shape_body,
                             contacts.rigid_contact_count,
                             contacts.rigid_contact_point0,
@@ -406,15 +406,14 @@ class FeatherstoneSolver(SolverBase):
                             contacts.rigid_contact_normal,
                             contacts.rigid_contact_shape0,
                             contacts.rigid_contact_shape1,
+                            contacts.rigid_contact_thickness0,
+                            contacts.rigid_contact_thickness1,
                             True,
                             self.friction_smoothing,
                         ],
                         outputs=[body_f],
                         device=model.device,
                     )
-
-                    # if model.rigid_contact_count.numpy()[0] > 0:
-                    #     print(body_f.numpy())
 
                 if model.articulation_count:
                     # evaluate joint torques
