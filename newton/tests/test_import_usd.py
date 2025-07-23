bf7b80f0c564a52896da8ab91f503bf2b4137a82
Eric Heiden <eric-heiden@outlook.com>
2025-07-22 22:26:47 -0700

    Add mesh approximation test for USD importer
    Signed-off-by: Eric Heiden <eric-heiden@outlook.com>

--- newton/tests/test_import_usd.py
+++ newton/tests/test_import_usd.py
@@ -21,7 +21,7 @@ import warp as wp
 
 import newton
 from newton.geometry.utils import create_box_mesh, transform_points
-from newton.tests.unittest_utils import USD_AVAILABLE, get_test_devices
+from newton.tests.unittest_utils import USD_AVAILABLE, assert_np_equal, get_test_devices
 from newton.utils import parse_usd
 
 devices = get_test_devices()
@@ -158,70 +158,79 @@ class TestImportUsd(unittest.TestCase):
 
     @unittest.skipUnless(USD_AVAILABLE, "Requires usd-core")
     def test_mesh_approximation(self):
-        from pxr import Gf, Usd, UsdGeom, UsdPhysics
+        from pxr import Gf, Usd, UsdGeom, UsdPhysics  # noqa: PLC0415
 
         def box_mesh(scale=(1.0, 1.0, 1.0), transform: wp.transform | None = None):
             vertices, indices = create_box_mesh(scale)
             if transform is not None:
                 vertices = transform_points(vertices, transform)
-            return newton.Mesh(vertices, indices)
+            return (vertices, indices)
+
+        def create_collision_mesh(name, vertices, indices, approximation_method):
+            mesh = UsdGeom.Mesh.Define(stage, name)
+            UsdPhysics.CollisionAPI.Apply(mesh.GetPrim())
+
+            mesh.CreateFaceVertexCountsAttr().Set([3] * (len(indices) // 3))
+            mesh.CreateFaceVertexIndicesAttr().Set(indices.tolist())
+            mesh.CreatePointsAttr().Set([Gf.Vec3f(*p) for p in vertices.tolist()])
+            mesh.CreateDoubleSidedAttr().Set(False)
+
+            prim = mesh.GetPrim()
+            meshColAPI = UsdPhysics.MeshCollisionAPI.Apply(prim)
+            meshColAPI.GetApproximationAttr().Set(approximation_method)
+            return prim
 
         def npsorted(x):
             return np.array(sorted(x))
 
         stage = Usd.Stage.CreateInMemory()
+        UsdGeom.SetStageUpAxis(stage, UsdGeom.Tokens.z)
+        UsdGeom.SetStageMetersPerUnit(stage, 1.0)
         self.assertTrue(stage)
 
         scene = UsdPhysics.Scene.Define(stage, "/physicsScene")
         self.assertTrue(scene)
 
-        tf = wp.transform(wp.vec3(1.0, 2.0, 3.0), wp.quat_identity())
         scale = wp.vec3(1.0, 3.0, 0.2)
+        tf = wp.transform(wp.vec3(1.0, 2.0, 3.0), wp.quat_identity())
+        vertices, indices = box_mesh(scale=scale, transform=tf)
 
-        rigidbody = UsdGeom.Xform.Define(stage, "/rigidBody")
-        UsdPhysics.RigidBodyAPI.Apply(rigidbody.GetPrim())
-        position = Gf.Vec3f(1.0, 2.0, -4.0)
-        rotate_xyz = Gf.Vec3f(0.0, 0.0, 45.0)
-
-        rigidbody.AddTranslateOp().Set(position)
-        rigidbody.AddRotateXYZOp().Set(rotate_xyz)
-        rigidbody.AddScaleOp().Set(Gf.Vec3f(*scale))
-
-        mesh = UsdGeom.Mesh.Define(stage, "/meshConvexHull")
-        UsdPhysics.CollisionAPI.Apply(mesh.GetPrim())
-
-        box = box_mesh(scale=scale)
-
-        # Fill in VtArrays
-        points = []
-        indices = []
-        vertex_counts = []
-
-        points = [Gf.Vec3f(*p) for p in box.vertices.tolist()]
-        indices = box.indices.tolist()
-        vertex_counts = [3] * (len(indices) // 3)
-
-        mesh.CreateFaceVertexCountsAttr().Set(vertex_counts)
-        mesh.CreateFaceVertexIndicesAttr().Set(indices)
-        mesh.CreatePointsAttr().Set(points)
-        mesh.CreateDoubleSidedAttr().Set(False)
-
-        mesh_prim = mesh.GetPrim()
-
-        meshColAPI = UsdPhysics.MeshCollisionAPI.Apply(mesh_prim)
-        meshColAPI.GetApproximationAttr().Set(UsdPhysics.Tokens.convexHull)
+        create_collision_mesh("/meshOriginal", vertices, indices, UsdPhysics.Tokens.none)
+        create_collision_mesh("/meshConvexHull", vertices, indices, UsdPhysics.Tokens.convexHull)
+        create_collision_mesh("/meshBoundingSphere", vertices, indices, UsdPhysics.Tokens.boundingSphere)
+        create_collision_mesh("/meshBoundingCube", vertices, indices, UsdPhysics.Tokens.boundingCube)
 
         builder = newton.ModelBuilder()
-        newton.geometry.MESH_MAXHULLVERT = 6
+        newton.geometry.MESH_MAXHULLVERT = 4
         parse_usd(
             stage,
             builder,
         )
 
-        self.assertEqual(builder.body_count, 1)
-        self.assertEqual(builder.shape_count, 1)
-        converted_mesh = builder.shape_geo_src[0]
-        self.assertEqual(converted_mesh.vertices.shape, (6, 3))
+        self.assertEqual(builder.body_count, 0)
+        self.assertEqual(builder.shape_count, 4)
+        self.assertEqual(builder.shape_geo_type, [newton.GEO_MESH, newton.GEO_MESH, newton.GEO_SPHERE, newton.GEO_BOX])
+
+        # original mesh
+        mesh_original = builder.shape_geo_src[0]
+        self.assertEqual(mesh_original.vertices.shape, (8, 3))
+        assert_np_equal(mesh_original.vertices, vertices)
+        assert_np_equal(mesh_original.indices, indices)
+
+        # convex hull
+        mesh_convex_hull = builder.shape_geo_src[1]
+        self.assertEqual(mesh_convex_hull.vertices.shape, (4, 3))
+
+        # bounding sphere
+        self.assertIsNone(builder.shape_geo_src[2])
+        self.assertEqual(builder.shape_geo_type[2], newton.geometry.GEO_SPHERE)
+        self.assertAlmostEqual(builder.shape_geo_scale[2][0], wp.length(scale))
+        assert_np_equal(np.array(builder.shape_transform[2].p), np.array(tf.p), tol=1.0e-4)
+
+        # bounding box
+        assert_np_equal(npsorted(builder.shape_geo_scale[3]), npsorted(scale), tol=1.0e-6)
+        # only compare the position since the rotation is not guaranteed to be the same
+        assert_np_equal(np.array(builder.shape_transform[3].p), np.array(tf.p), tol=1.0e-4)
 
 
 if __name__ == "__main__":
