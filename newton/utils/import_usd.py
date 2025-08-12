5ca1ce10e5b9f55c10ed47cd9da60acff19dc49f
Viktor Reutskyy <vreutskyy@nvidia.com>
2025-08-12 14:00:01 +0200

    Skip a joint if none of the bodies are set
    Signed-off-by: Viktor Reutskyy <vreutskyy@nvidia.com>

--- newton/utils/import_usd.py
+++ newton/utils/import_usd.py
@@ -517,14 +517,16 @@ def parse_usd(
         child_tf = wp.transform(joint_desc.localPose1Position, from_gfquat(joint_desc.localPose1Orientation))
         # If child_id is -1, swap parent and child
         if child_id == -1:
+            if parent_id == -1:
+                if verbose:
+                    print(f"Skipping joint {joint_path}: both bodies unresolved")
+                return
             parent_id, child_id = child_id, parent_id
             parent_tf, child_tf = child_tf, parent_tf
             if verbose:
                 print(f"Joint {joint_path} connects {parent_path} to world")
         if incoming_xform is not None:
             parent_tf = wp.mul(incoming_xform, parent_tf)
-        # if parent_id == -1:
-        #     print("joint connected to world")
 
         joint_armature = parse_float(joint_prim, "physxJoint:armature", default_joint_armature)
         joint_params = {
