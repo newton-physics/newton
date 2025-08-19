978ba7fba077dc659ffa7abf9099351ea1a82525
Julia von Muralt <jvonmuralt@nvidia.com>
2025-08-19 09:34:19 +0200

    cleanup
    Signed-off-by: Julia von Muralt <jvonmuralt@nvidia.com>

--- newton/examples/example_anymal_d.py
+++ newton/examples/example_anymal_d.py
@@ -16,10 +16,10 @@
 ###########################################################################
 # Example Anymal D walk
 #
-# Shows how to control Anymal D with a policy pretrained in physx.
+# Shows how to control Anymal D with multiple environments.
 #
 # Example usage:
-# uv run  newton/examples/example_anymal_d.py
+# uv run  newton/examples/example_anymal_d.py --num-envs 4
 #
 ###########################################################################
 
@@ -33,26 +33,48 @@ import newton.utils
 
 
 class Example:
-    def __init__(self, stage_path=None, headless=False):
+    def __init__(self, stage_path=None, headless=False, num_envs=8, use_cuda_graph=True):
         self.device = wp.get_device()
+        self.num_envs = num_envs
 
-        builder = newton.ModelBuilder(up_axis=newton.Axis.Z)
-        builder.default_joint_cfg = newton.ModelBuilder.JointDofConfig(limit_ke=1.0e3, limit_kd=1.0e1, friction=1e-5)
-        builder.default_shape_cfg.ke = 5.0e4
-        builder.default_shape_cfg.kd = 5.0e2
-        builder.default_shape_cfg.kf = 1.0e3
-        builder.default_shape_cfg.mu = 0.75
+        articulation_builder = newton.ModelBuilder(up_axis=newton.Axis.Z)
+        articulation_builder.default_joint_cfg = newton.ModelBuilder.JointDofConfig(
+            limit_ke=1.0e3, limit_kd=1.0e1, friction=1e-5
+        )
+        articulation_builder.default_shape_cfg.ke = 5.0e4
+        articulation_builder.default_shape_cfg.kd = 5.0e2
+        articulation_builder.default_shape_cfg.kf = 1.0e3
+        articulation_builder.default_shape_cfg.mu = 0.75
 
         if stage_path is None:
             asset_path = newton.utils.download_asset("anymal_usd")
             stage_path = str(asset_path / "anymal_d.usda")
         newton.utils.parse_usd(
             stage_path,
-            builder,
+            articulation_builder,
             collapse_fixed_joints=False,
             enable_self_collisions=False,
             load_non_physics_prims=False,
         )
+
+        articulation_builder.joint_q[:3] = [0.0, 0.0, 0.62]
+        if len(articulation_builder.joint_q) > 6:
+            articulation_builder.joint_q[3:7] = [0.0, 0.0, 0.0, 1.0]
+
+        for i in range(len(articulation_builder.joint_dof_mode)):
+            articulation_builder.joint_dof_mode[i] = newton.JointMode.TARGET_POSITION
+
+        for i in range(len(articulation_builder.joint_target_ke)):
+            articulation_builder.joint_target_ke[i] = 150
+            articulation_builder.joint_target_kd[i] = 5
+
+        spacing = 3.0
+        sqn = int(wp.ceil(wp.sqrt(float(self.num_envs))))
+
+        builder = newton.ModelBuilder(up_axis=newton.Axis.Z)
+        for i in range(self.num_envs):
+            pos = wp.vec3((i % sqn) * spacing, (i // sqn) * spacing, 0)
+            builder.add_builder(articulation_builder, xform=wp.transform(pos, wp.quat_identity()))
 
         builder.add_ground_plane()
 
@@ -64,15 +86,6 @@ class Example:
         self.sim_substeps = 4
         self.sim_dt = self.frame_dt / self.sim_substeps
 
-        builder.joint_q[:3] = [0.0, 0.0, 0.62]
-
-        for i in range(len(builder.joint_dof_mode)):
-            builder.joint_dof_mode[i] = newton.JointMode.TARGET_POSITION
-
-        for i in range(len(builder.joint_target_ke)):
-            builder.joint_target_ke[i] = 150
-            builder.joint_target_kd[i] = 5
-
         self.model = builder.finalize()
         self.solver = newton.solvers.SolverMuJoCo(
             self.model, cone=mujoco.mjtCone.mjCONE_ELLIPTIC, impratio=100, iterations=100, ls_iterations=50
@@ -83,28 +96,25 @@ class Example:
         self.state_0 = self.model.state()
         self.state_1 = self.model.state()
         self.control = self.model.control()
-        self.contacts = self.model.collide(self.state_0, rigid_contact_margin=0.1)
+        self.contacts = None
         newton.eval_fk(self.model, self.state_0.joint_q, self.state_0.joint_qd, self.state_0)
 
-        self.use_cuda_graph = self.device.is_cuda and wp.is_mempool_enabled(wp.get_device())
+        self.use_cuda_graph = self.device.is_cuda and wp.is_mempool_enabled(wp.get_device()) and use_cuda_graph
+
         if self.use_cuda_graph:
-            warp_array = wp.zeros(18, dtype=wp.float32, device=self.device)
-            self.control.joint_target = warp_array
             with wp.ScopedCapture() as capture:
                 self.simulate()
             self.graph = capture.graph
-        else:
-            self.graph = None
 
     def simulate(self):
-        self.contacts = self.model.collide(self.state_0, rigid_contact_margin=0.1)
+        self.contacts = None
         for _ in range(self.sim_substeps):
             self.state_0.clear_forces()
             self.solver.step(self.state_0, self.state_1, self.control, self.contacts, self.sim_dt)
             self.state_0, self.state_1 = self.state_1, self.state_0
 
     def step(self):
-        with wp.ScopedTimer("step"):
+        with wp.ScopedTimer("step", active=False):
             if self.use_cuda_graph:
                 wp.capture_launch(self.graph)
             else:
@@ -115,7 +125,7 @@ class Example:
         if self.renderer is None:
             return
 
-        with wp.ScopedTimer("render"):
+        with wp.ScopedTimer("render", active=False):
             self.renderer.begin_frame(self.sim_time)
             self.renderer.render(self.state_0)
             self.renderer.end_frame()
@@ -132,16 +142,26 @@ if __name__ == "__main__":
         help="Path to the output URDF file.",
     )
     parser.add_argument("--num-frames", type=int, default=1000, help="Total number of frames.")
+    parser.add_argument("--num-envs", type=int, default=8, help="Total number of simulated environments.")
     parser.add_argument("--headless", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--use-cuda-graph", default=True, action=argparse.BooleanOptionalAction)
 
     args = parser.parse_known_args()[0]
 
     with wp.ScopedDevice(args.device):
-        example = Example(stage_path=args.stage_path, headless=args.headless)
+        example = Example(
+            stage_path=args.stage_path,
+            headless=args.headless,
+            num_envs=args.num_envs,
+            use_cuda_graph=args.use_cuda_graph,
+        )
 
-        for _ in range(args.num_frames):
+        for frame_idx in range(args.num_frames):
             example.step()
             example.render()
+
+            if example.renderer is None:
+                print(f"[{frame_idx:4d}/{args.num_frames}]")
 
         if example.renderer:
             example.renderer.save()
