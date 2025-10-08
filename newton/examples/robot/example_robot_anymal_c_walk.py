2de2670b0be9f6626555eb87a11955f73ce71936
Lennart Roestel <lroestel@nvidia.com>
2025-10-08 21:04:54 +0200

    add walking direction tests for anymal examples

--- newton/examples/robot/example_robot_anymal_c_walk.py
+++ newton/examples/robot/example_robot_anymal_c_walk.py
@@ -235,8 +235,16 @@ class Example:
             "all bodies are above the ground",
             lambda q, qd: q[2] > 0.1,
         )
-        forward_vel_min = wp.spatial_vector(-0.2, 0.9, -0.2, -0.8, -0.5, -0.5)
-        forward_vel_max = wp.spatial_vector(0.2, 1.1, 0.2, 0.8, 0.5, 0.5)
+
+        newton.examples.test_body_state(
+            self.model,
+            self.state_0,
+            "the robot went in the right direction",
+            lambda q, qd: q[1] > 9.0,  # This threshold assumes 500 frames
+        )
+
+        forward_vel_min = wp.spatial_vector(-0.5, 0.9, -0.2, -0.8, -0.5, -0.5)
+        forward_vel_max = wp.spatial_vector(0.5, 1.1, 0.2, 0.8, 0.5, 0.5)
         newton.examples.test_body_state(
             self.model,
             self.state_0,
