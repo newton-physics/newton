dc070e37f14905bdbcc559ce736a5bd0fe33287a
Kenny Vilella <kvilella@nvidia.com>
2025-10-21 13:46:54 +0800

    Autoformatter

--- newton/tests/test_anymal_reset.py
+++ newton/tests/test_anymal_reset.py
@@ -101,7 +101,14 @@ class TestAnymalReset(unittest.TestCase):
             impratio = 100.0
 
         self.solver = newton.solvers.SolverMuJoCo(
-            self.model, solver=2, cone=cone_type, impratio=impratio, iterations=100, ls_iterations=50, njmax=200, ncon_per_env=50,
+            self.model,
+            solver=2,
+            cone=cone_type,
+            impratio=impratio,
+            iterations=100,
+            ls_iterations=50,
+            njmax=200,
+            ncon_per_env=50,
         )
 
         if self.headless:
