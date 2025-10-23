e048316e0880517a5211c035472bf8e86361a27a
Gilles Daviet <gdaviet@nvidia.com>
2025-10-23 17:18:52 +0200

    Fix max_pairs in unified collision pipeline

--- newton/_src/sim/collide_unified.py
+++ newton/_src/sim/collide_unified.py
@@ -329,6 +329,8 @@ class CollisionPipelineUnified:
         self.enable_contact_matching = enable_contact_matching
         self.reduce_contacts = reduce_contacts
         self.shape_pairs_max = (shape_count * (shape_count - 1)) // 2
+        if broad_phase_mode == BroadPhaseMode.EXPLICIT:
+            self.shape_pairs_max = min(self.shape_pairs_max, len(shape_pairs_filtered))
 
         # Initialize broad phase
         if self.broad_phase_mode == BroadPhaseMode.NXN:
