ce997baf321562944acc0ee61de24060a8da7450
Eric Heiden <eric-heiden@outlook.com>
2025-11-14 09:35:26 -0800

    Update notebook, run script
    Signed-off-by: Eric Heiden <eric-heiden@outlook.com>

--- brev_runscript.sh
+++ brev_runscript.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-cd newton
-
-# activate the Jupyter virtual environment
+echo "Activating Jupyter virtual environment"
 source /home/ubuntu/.venv/bin/activate
 
-# install uv
+echo "Installing uv"
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-# install dependencies into the currently active venv
-/home/ubuntu/.local/bin/uv sync --extra notebook --active
+cd /home/ubuntu/newton
+VIRTUAL_ENV=/home/ubuntu/.venv /home/ubuntu/.local/bin/uv sync --extra notebook --active
+
+echo "Running robot_policy example"
+/home/ubuntu/.local/bin/uv run --extra torch-cu12 -m newton.examples robot_policy --viewer null
+
+# fix messed up Jupyter Lab installation
+VIRTUAL_ENV=/home/ubuntu/.venv /home/ubuntu/.local/bin/uv pip install --upgrade --force-reinstall --no-cache-dir jupyter
+
+# force restart of Jupyter Lab (somehow Brev will restart Jupyter Lab automatically)
+pkill jupyter
