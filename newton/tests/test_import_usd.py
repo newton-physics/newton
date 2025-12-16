6925a7d83675f302916751639f7b1dbcdf7477e8
Gordon Yeoman <gyeoman@nvidia.com>
2025-12-16 11:41:04 +0100

    Removing noqa: PLC0415 comments, as suggested by coderabbit Signed-off-by: Gordon Yeoman <gyeoman@nvidia.com>

--- newton/tests/test_import_usd.py
+++ newton/tests/test_import_usd.py
@@ -1428,9 +1428,9 @@ def PhysicsRevoluteJoint "Joint2"
     @unittest.skipUnless(USD_AVAILABLE, "Requires usd-core")
     def test_geom_margin_parsing(self):
         """Test that geom_margin attribute is parsed correctly from USD."""
-        from pxr import Usd  # noqa: PLC0415
+        from pxr import Usd
 
-        from newton._src.usd.schemas import SchemaResolverMjc  # noqa: PLC0415
+        from newton._src.usd.schemas import SchemaResolverMjc
 
         usd_content = """#usda 1.0
 (
