18228581a5a5208cb8c7e0e10e8662f635a25499
Gordon Yeoman <gyeoman@nvidia.com>
2025-12-18 10:20:27 +0100

    Use parse_float Signed-off-by: Gordon Yeoman <gyeoman@nvidia.com>

--- newton/_src/utils/import_mjcf.py
+++ newton/_src/utils/import_mjcf.py
@@ -334,8 +334,7 @@ def parse_mjcf(
                     shape_cfg.rolling_friction = float(friction_values[2])
 
             # Parse MJCF margin (contact margin for collision detection)
-            if "margin" in geom_attrib:
-                shape_cfg.contact_margin = float(geom_attrib["margin"])
+            shape_cfg.contact_margin = parse_float(geom_attrib, "margin", shape_cfg.contact_margin)
 
             custom_attributes = parse_custom_attributes(geom_attrib, builder_custom_attr_shape, parsing_mode="mjcf")
             shape_kwargs = {
