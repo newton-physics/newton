fa32bd28503eea92f98925127b83debdfae2d02e
Eric Heiden <eheiden@nvidia.com>
2026-01-05 15:30:22 +0100

    Create newton.math module
    Signed-off-by: Eric Heiden <eheiden@nvidia.com>

---
+++ newton/math.py
@@ -0,0 +1,49 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any
+
+import numpy as np
+import warp as wp
+
+from ._src.math import (
+    boltzmann,
+    smooth_max,
+    smooth_min,
+    leaky_max,
+    leaky_min,
+    vec_min,
+    vec_max,
+    vec_leaky_min,
+    vec_leaky_max,
+    vec_abs,
+    vec_allclose,
+    vec_inside_limits,
+)
+
+__all__ = [
+    "boltzmann",
+    "smooth_max",
+    "smooth_min",
+    "leaky_max",
+    "leaky_min",
+    "vec_min",
+    "vec_max",
+    "vec_leaky_min",
+    "vec_leaky_max",
+    "vec_abs",
+    "vec_allclose",
+    "vec_inside_limits",
+]
