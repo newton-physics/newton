281fcfa49e08c0480869870e6910b7177f5fd295
Eric Heiden <eric-heiden@outlook.com>
2026-01-26 23:23:38 -0800

    fix(utils): restore math exports and imports

--- newton/_src/utils/__init__.py
+++ newton/_src/utils/__init__.py
@@ -19,6 +19,20 @@ import numpy as np
 from warp.context import assert_conditional_graph_support
 
 from ..core.types import Axis
+from ..math import (
+    boltzmann,
+    leaky_max,
+    leaky_min,
+    smooth_max,
+    smooth_min,
+    vec_abs,
+    vec_allclose,
+    vec_inside_limits,
+    vec_leaky_max,
+    vec_leaky_min,
+    vec_max,
+    vec_min,
+)
 from .download_assets import clear_git_cache, download_asset
 from .topology import topological_sort
 
