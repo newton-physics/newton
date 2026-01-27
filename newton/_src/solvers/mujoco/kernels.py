8f425021c9ab72a6c5297ab4250e953d5ddf613f
Viktor Reutskyy <vreutskyy@nvidia.com>
2026-01-27 13:48:33 +0100

    A follow up fix for #1192
    Signed-off-by: Viktor Reutskyy <vreutskyy@nvidia.com>

--- newton/_src/solvers/mujoco/kernels.py
+++ newton/_src/solvers/mujoco/kernels.py
@@ -1505,8 +1505,8 @@ def update_eq_data_and_active_kernel(
 
     constraint_type = eq_constraint_type[newton_eq]
 
-    # Initialize output data
-    data = vec11(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    # Read existing data to preserve fields we don't update
+    data = eq_data_out[world, mjc_eq]
 
     if constraint_type == int(EqType.CONNECT):
         # CONNECT: data[0:3] = anchor
