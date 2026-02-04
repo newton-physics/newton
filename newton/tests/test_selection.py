8450bc6c86edf75a956d05bdec1b5e2141461947
Gordon Yeoman <gyeoman@nvidia.com>
2026-02-04 14:28:43 +0100

    1st attempt at tests of selection API with joints and links Signed-off-by: Gordon Yeoman <gyeoman@nvidia.com>

--- newton/tests/test_selection.py
+++ newton/tests/test_selection.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import unittest
 
 import numpy as np
@@ -21,6 +22,7 @@ import warp as wp
 import newton
 import newton.examples
 from newton.selection import ArticulationView
+from newton.solvers import SolverMuJoCo
 from newton.tests.unittest_utils import assert_np_equal
 
 
@@ -189,6 +191,301 @@ class TestSelection(unittest.TestCase):
         model_mask = view.get_model_articulation_mask(mask=m)
         expected = np.array([0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0], dtype=np.bool)
         assert_np_equal(model_mask.numpy(), expected)
+
+    def run_test_joint_selection(self, use_mask: bool):
+        """Test an ArticulationView that includes a subset of joints and that we
+        can write attributes to the subset of joints with and without a mask. Test
+        that we can write to model/state/control."""
+        mjcf = """<?xml version="1.0" ?>
+<mujoco model="myart">
+    <worldbody>
+    <!-- Root body (fixed to world) -->
+    <body name="root" pos="0 0 0">
+
+      <!-- First child link with prismatic joint along x -->
+      <body name="link1" pos="0.0 -0.5 0">
+        <joint name="joint1" type="slide" axis="1 0 0" range="-50.5 50.5"/>
+        <inertial pos="0 0 0" mass="1.0" diaginertia="0.01 0.01 0.01"/>
+      </body>
+
+      <!-- Second child link with prismatic joint along x -->
+      <body name="link2" pos="-0.0 -0.7 0">
+        <joint name="joint2" type="slide" axis="1 0 0" range="-50.5 50.5"/>
+        <inertial pos="0 0 0" mass="1.0" diaginertia="0.01 0.01 0.01"/>
+      </body>
+
+      <!-- Third child link with prismatic joint along x -->
+      <body name="link3" pos="-0.0 -0.9 0">
+        <joint name="joint3" type="slide" axis="1 0 0" range="-50.5 50.5"/>
+        <inertial pos="0 0 0" mass="1.0" diaginertia="0.01 0.01 0.01"/>
+      </body>
+    </body>
+  </worldbody>
+</mujoco>
+"""
+
+        num_joints_per_world = 3
+        num_worlds = 3
+        num_joints = num_joints_per_world * num_worlds
+        art_name = "myart"
+
+        # Create a single articulation
+        individual_builder = newton.ModelBuilder()
+        SolverMuJoCo.register_custom_attributes(individual_builder)
+        individual_builder.add_mjcf(mjcf)
+
+        # Create 3 worlds with a single articulation per world.
+        builder = newton.ModelBuilder()
+        for _i in range(0, num_worlds):
+            builder.add_world(individual_builder)
+
+        # Create the model
+        model = builder.finalize()
+        state_0 = model.state()
+        control = model.control()
+
+        # Create a view of "joint3"
+        joints_to_include = ["joint3"]
+        joint_view = ArticulationView(model, art_name, include_joints=joints_to_include)
+
+        # create a view of "link1" and "link2"
+        links_to_exclude = ["root", "link3"]
+        link_view = ArticulationView(model, art_name, exclude_links=links_to_exclude)
+
+        # Get the attributes associated with "joint3"
+        joint_dof_positions = joint_view.get_dof_positions(model).numpy()
+        joint_limit_lower = joint_view.get_attribute("joint_limit_lower", model).numpy()
+        joint_target_pos = joint_view.get_attribute("joint_target_pos", model).numpy()
+
+        # Modify the attributes associated with "joint3"
+        val = 1.0
+        for world_idx in range(joint_dof_positions.shape[0]):
+            for arti_idx in range(joint_dof_positions.shape[1]):
+                for joint_idx in range(joint_dof_positions.shape[2]):
+                    joint_dof_positions[world_idx, arti_idx, joint_idx] = val
+                    joint_limit_lower[world_idx, arti_idx, joint_idx] += val
+                    joint_target_pos[world_idx, arti_idx, joint_idx] += 2.0 * val
+                    val += 1.0
+
+        mask = None
+        if use_mask:
+            mask = [[False], [False], [True]]
+
+        expected_dof_positions = []
+        expected_joint_limit_lower = []
+        expected_joint_target_pos = []
+        if use_mask:
+            expected_dof_positions = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0]
+            expected_joint_limit_lower = [-50.5, -50.5, -50.5, -50.5, -50.5, -50.5, -50.5, -50.5, -47.5]
+            expected_joint_target_pos = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 6.0]
+        else:
+            expected_dof_positions = [0.0, 0.0, 1.0, 0.0, 0.0, 2.0, 0.0, 0.0, 3.0]
+            expected_joint_limit_lower = [-50.5, -50.5, -49.5, -50.5, -50.5, -48.5, -50.5, -50.5, -47.5]
+            expected_joint_target_pos = [0.0, 0.0, 2.0, 0.0, 0.0, 4.0, 0.0, 0.0, 6.0]
+
+        # Set the values associated with "joint3"
+        joint_view.set_dof_positions(state_0, joint_dof_positions, mask)
+        joint_view.set_dof_positions(model, joint_dof_positions, mask)
+        joint_view.set_attribute("joint_limit_lower", model, joint_limit_lower, mask)
+        joint_view.set_attribute("joint_target_pos", control, joint_target_pos, mask)
+        joint_view.set_attribute("joint_target_pos", model, joint_target_pos, mask)
+
+        # Get the updated values from model, state, control.
+        measured_state_joint_dof_positions = state_0.joint_q.numpy()
+        measured_model_joint_dof_positions = model.joint_q.numpy()
+        measured_model_joint_limit_lower = model.joint_limit_lower.numpy()
+        measured_control_joint_target_pos = control.joint_target_pos.numpy()
+        measured_model_joint_target_pos = model.joint_target_pos.numpy()
+
+        # Test that the modified values were correctly set in model, state and control
+        for i in range(0, num_joints):
+            measured = measured_state_joint_dof_positions[i]
+            expected = expected_dof_positions[i]
+            self.assertAlmostEqual(
+                expected,
+                measured,
+                places=4,
+                msg=f"Expected state joint dof position value: {expected}, Measured value: {measured}",
+            )
+
+            measured = measured_model_joint_dof_positions[i]
+            expected = expected_dof_positions[i]
+            self.assertAlmostEqual(
+                expected,
+                measured,
+                places=4,
+                msg=f"Expected model joint dof position value: {expected}, Measured value: {measured}",
+            )
+
+            measured = measured_model_joint_limit_lower[i]
+            expected = expected_joint_limit_lower[i]
+            self.assertAlmostEqual(
+                expected,
+                measured,
+                places=4,
+                msg=f"Expected model joint limit lower value: {expected}, Measured value: {measured}",
+            )
+
+            measured = measured_control_joint_target_pos[i]
+            expected = expected_joint_target_pos[i]
+            self.assertAlmostEqual(
+                expected,
+                measured,
+                places=4,
+                msg=f"Expected model joint target pos value: {expected}, Measured value: {measured}",
+            )
+
+            measured = measured_model_joint_target_pos[i]
+            expected = expected_joint_target_pos[i]
+            self.assertAlmostEqual(
+                expected,
+                measured,
+                places=4,
+                msg=f"Expected model joint target pos value: {expected}, Measured value: {measured}",
+            )
+
+    def run_test_link_selection(self, use_mask: bool):
+        """Test an ArticulationView that excludes a subset of links and that we
+        can write attributes to the subset of links with and without a mask"""
+        mjcf = """<?xml version="1.0" ?>
+<mujoco model="myart">
+    <worldbody>
+    <!-- Root body (fixed to world) -->
+    <body name="root" pos="0 0 0">
+      <!-- First child link with prismatic joint along x -->
+      <body name="link1" pos="0.0 -0.5 0">
+        <joint name="joint1" type="slide" axis="1 0 0" range="-50.5 50.5"/>
+        <inertial pos="0 0 0" mass="1" diaginertia="0.01 0.01 0.01"/>
+      </body>
+
+      <!-- Second child link with prismatic joint along x -->
+      <body name="link2" pos="-0.0 -0.7 0">
+        <joint name="joint2" type="slide" axis="1 0 0" range="-50.5 50.5"/>
+        <inertial pos="0 0 0" mass="1" diaginertia="0.01 0.01 0.01"/>
+      </body>
+
+      <!-- Third child link with prismatic joint along x -->
+      <body name="link3" pos="-0.0 -0.9 0">
+        <joint name="joint3" type="slide" axis="1 0 0" range="-50.5 50.5"/>
+        <inertial pos="0 0 0" mass="1" diaginertia="0.01 0.01 0.01"/>
+      </body>
+    </body>
+  </worldbody>
+</mujoco>
+"""
+        num_links_per_articulation = 4
+        num_articulations_per_world = 1
+        num_worlds = 3
+        num_links = num_links_per_articulation * num_articulations_per_world * num_worlds
+
+        # Create a single articulation
+        individual_builder = newton.ModelBuilder()
+        SolverMuJoCo.register_custom_attributes(individual_builder)
+        individual_builder.add_mjcf(mjcf)
+
+        # Create 3 worlds with a single articulation per world.
+        builder = newton.ModelBuilder()
+        for _i in range(0, num_worlds):
+            builder.add_world(individual_builder)
+
+        # Create the model
+        model = builder.finalize()
+
+        # create a view of "link1" and "link2" by excluding "root" and "link3"
+        links_to_exclude = ["root", "link3"]
+        link_view = ArticulationView(model, "myart", exclude_links=links_to_exclude)
+
+        # Get the attributes associated with "link1" and "link2"
+        link_masses = link_view.get_attribute("body_mass", model).numpy()
+        link_vels = link_view.get_attribute("body_qd", model).numpy()
+
+        # Modify the attributes associated with "link1" and "link2"
+        val = 1.0
+        for world_idx in range(link_masses.shape[0]):
+            for arti_idx in range(link_masses.shape[1]):
+                for link_idx in range(link_masses.shape[2]):
+                    link_masses[world_idx, arti_idx, link_idx] += val
+                    link_vels[world_idx, arti_idx, link_idx] = [val, val, val, val, val, val]
+                    val += 1.0
+
+        mask = None
+        if use_mask:
+            mask = [[False], [True], [False]]
+        link_view.set_attribute("body_mass", model, link_masses, mask)
+        link_view.set_attribute("body_qd", model, link_vels, mask)
+
+        expected_body_masses = []
+        expected_body_vels = []
+        if use_mask:
+            expected_body_masses = [0.01, 0.01, 0.01, 0.01, 0.01, 3.01, 4.01, 0.01, 0.01, 0.01, 0.01, 0.01]
+            expected_body_vels = [
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [3.0, 3.0, 3.0, 3.0, 3.0, 3.0],
+                [4.0, 4.0, 4.0, 4.0, 4.0, 4.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            ]
+
+        else:
+            expected_body_masses = [0.01, 1.01, 2.01, 0.01, 0.01, 3.01, 4.01, 0.01, 0.01, 5.01, 6.01, 0.01]
+            expected_body_vels = [
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                [2.0, 2.0, 2.0, 2.0, 2.0, 2.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [3.0, 3.0, 3.0, 3.0, 3.0, 3.0],
+                [4.0, 4.0, 4.0, 4.0, 4.0, 4.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [5.0, 5.0, 5.0, 5.0, 5.0, 5.0],
+                [6.0, 6.0, 6.0, 6.0, 6.0, 6.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            ]
+
+        # Get the updated body masses
+        measured_body_masses = model.body_mass.numpy()
+        measured_body_vels = model.body_qd.numpy()
+
+        # Test that the modified values were correctly set in model
+        for i in range(0, num_links):
+            measured = measured_body_masses[i]
+            expected = expected_body_masses[i]
+            self.assertAlmostEqual(
+                expected,
+                measured,
+                places=4,
+                msg=f"Expected body mass value: {expected}, Measured value: {measured}",
+            )
+
+            for j in range(0, 6):
+                measured = measured_body_vels[i][j]
+                expected = expected_body_vels[i][j]
+                self.assertAlmostEqual(
+                    expected,
+                    measured,
+                    places=4,
+                    msg=f"Expected body velocity value: {expected}, Measured value: {measured}",
+                )
+
+    def test_joint_selection_no_mask(self):
+        self.run_test_joint_selection(False)
+
+    def test_joint_selection_with_mask(self):
+        self.run_test_joint_selection(True)
+
+    def test_link_selection_no_mask(self):
+        self.run_test_link_selection(False)
+
+    def test_link_selection_with_mask(self):
+        self.run_test_link_selection(True)
 
 
 if __name__ == "__main__":
