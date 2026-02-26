6d8f8f4d6b4b76aa38dbf37d590a19009648f7b4
Eric Heiden <eric-heiden@outlook.com>
2026-02-25 17:46:24 -0800

    Refactor mesh logging in ViewerRerun to improve handling of hidden meshes. Ensure hidden meshes retain shading data (normals, uvs, texture) for later instancing. Update default viewer to 'rerun' in argument parser. Add tests to verify hidden mesh behavior.
    Signed-off-by: Eric Heiden <eric-heiden@outlook.com>

--- newton/examples/__init__.py
+++ newton/examples/__init__.py
@@ -297,7 +297,7 @@ def create_parser():
     parser.add_argument(
         "--viewer",
         type=str,
-        default="gl",
+        default="rerun",
         choices=["gl", "usd", "rerun", "null", "viser"],
         help="Viewer to use (gl, usd, rerun, or null).",
     )
