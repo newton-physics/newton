41c970fb66fed2ffa0de8e2930da007fdf48bae2
Lukasz Wawrzyniak <lwawrzyniak@nvidia.com>
2026-03-24 17:24:23 -0400

    Revert "Fix a typo"
    This reverts commit 9e0ce13fac67aa5c4c7a44810f34d2afcf95e580.

--- newton/examples/robot/example_robot_policy.py
+++ newton/examples/robot/example_robot_policy.py
@@ -398,7 +398,7 @@ class Example:
             # shadow_builder.approximate_meshes("convex_hull")
 
             add_terrain(
-                shadow_builder,
+                builder,
                 file_path=background_usd_path,
                 root_prim=background_usd_root,
                 usd_collide=background_usd_collide,
