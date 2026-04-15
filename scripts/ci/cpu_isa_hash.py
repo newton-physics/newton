9cbc29718b896e4c96b16ab0afb6a0edbbf383a3
Alain Denzler <adenzler@nvidia.com>
2026-04-15 13:40:53 +0200

    Fix SPDX license header in cpu_isa_hash.py

--- scripts/ci/cpu_isa_hash.py
+++ scripts/ci/cpu_isa_hash.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2025 NVIDIA CORPORATION. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026 The Newton Developers
 # SPDX-License-Identifier: Apache-2.0
 
 """Print a short hash of the host CPU's ISA feature set.
