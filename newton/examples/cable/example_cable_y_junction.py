8bd6b07c95583fe1d60ca1b3de1b99555317a065
JC <jumyungc@nvidia.com>
2026-06-02 16:47:00 +1200

    Update cable example thumbnails
    Signed-off-by: JC <jumyungc@nvidia.com>

--- newton/examples/cable/example_cable_y_junction.py
+++ newton/examples/cable/example_cable_y_junction.py
@@ -38,19 +38,22 @@ class Example:
         self.fps = 60
         self.frame_dt = 1.0 / self.fps
         self.sim_time = 0.0
-        self.sim_substeps = 5
+        self.sim_substeps = 10
         self.sim_iterations = 5
         self.sim_dt = self.frame_dt / self.sim_substeps
 
         # Cable parameters.
         cable_radius = 0.01
+        contact_gap = 0.002
         num_segments_per_branch = 20
         segment_length = 0.03
 
-        bend_stiffness = 1.0e3
+        stretch_stiffness = 1.0e7
+        bend_stiffness = 1.0e4
         bend_damping = 1.0e-1
 
         builder = newton.ModelBuilder()
+        builder.rigid_gap = contact_gap
         builder.default_shape_cfg.ke = 1.0e4
         builder.default_shape_cfg.kd = 0.0
         builder.default_shape_cfg.mu = 1.0
@@ -80,6 +83,7 @@ class Example:
             edges=edges,
             radius=cable_radius,
             cfg=cable_cfg,
+            stretch_stiffness=stretch_stiffness,
             bend_stiffness=bend_stiffness,
             bend_damping=bend_damping,
             label="y_graph",
