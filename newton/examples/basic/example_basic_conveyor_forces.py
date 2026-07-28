ac639eddf8e776a464fc62606875cb556c4a1590
Julia von Muralt <jvonmuralt@nvidia.com>
2026-07-28 15:08:38 +0200

    fix

--- newton/examples/basic/example_basic_conveyor_forces.py
+++ newton/examples/basic/example_basic_conveyor_forces.py
@@ -1,4 +1,3 @@
-# SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-FileCopyrightText: Copyright (c) 2026 The Newton Developers
 # SPDX-License-Identifier: Apache-2.0
 
