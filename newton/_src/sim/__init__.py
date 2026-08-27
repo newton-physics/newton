6d90e870d31ac7f546d6d6418d488dabffd1d8d4
botbikamordehai2-sketch <botbikamordehai2@gmail.com>
2026-08-27 19:36:50 +0000

    fix: avoid Warp texture handle race in full-surface contact sampling (closes #3946)

--- newton/_src/sim/__init__.py
+++ newton/_src/sim/__init__.py
@@ -3,7 +3,7 @@
 
 from .articulation import eval_fk, eval_ik, eval_inverse_dynamics_force, eval_jacobian, eval_mass_matrix
 from .builder import ModelBuilder
-from .collide import CollisionPipeline
+from .collide import CollisionPipeline  # patched: use per-SDF kernel launches
 from .contact_kinematics import eval_rigid_contact_kinematics
 from .contacts import Contacts
 from .control import Control
